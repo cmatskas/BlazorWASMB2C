4ec6c02238e2b986e72ec8a298df23988b2c87f2
Awesome Hacker <awesome.hacker@test.com>
2021-06-14 09:11:24 -0700

    Added default authZ

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
